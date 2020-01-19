89218429f9a28792cfe05647536adf7e6a838026
Fabien1690 <fabien.bertin@telecom-sudparis.eu>
2020-01-19 20:01:31 +0100

    Fin test Notes : Modifier keras.json pour avoir "backend": "theano" et "image_data_format": "channels_first"

--- network.py
+++ network.py
@@ -13,6 +13,7 @@ and omits many desirable features.
 # Standard library
 import random
 import numpy as np
+from keras.datasets import mnist
 
 
 class Network(object):
@@ -62,10 +63,10 @@ class Network(object):
             for mini_batch in mini_batches:
                 self.update_mini_batch(mini_batch, eta)
             if test_data:
-                print "Epoch {0}: {1} / {2}".format(
-                    j, self.evaluate(test_data), n_test)
+                print ("Epoch {0}: {1} / {2}".format(
+                    j, self.evaluate(test_data), n_test))
             else:
-                print "Epoch {0} complete".format(j)
+                print ("Epoch {0} complete".format(j))
 
     def update_mini_batch(self, mini_batch, eta):
         """Update the network's weights and biases by applying
